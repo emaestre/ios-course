9c51a4ba9f443f0f62be1c6b7beed570cafdd2d9
Eleazar Maestre <eleazarenrique23@gmail.com>
2019-11-16 20:31:45 -0400

    Variable declaration.

--- week-2-playground.playground/Contents.swift
+++ week-2-playground.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
+/* Declaration of a variable without a type */
 var str = "Hello, playground"
+
+/* Declaration of a variable with data type */
+var other:String = "Hi, my name is other"
+
+/* Declaration of a variable with optional value */
+var new_str:String? = nil
+var a_new_str:String?
